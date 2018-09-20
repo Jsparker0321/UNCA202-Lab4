bb6ddb44e6cbe2bd4747b6f626f08fbf937c8f2c
jessp <jessp@10.120.188.225>
2018-09-19 23:24:37 -0400

    All classes written fully

--- Lab4/src/Driver.java
+++ Lab4/src/Driver.java
@@ -9,9 +9,16 @@ public class Driver {
 		System.out.println(p1.hashCode());
 		
 		//Create a UniversityMember
-		//Create a Professor
-		//Create a Student
+		UniversityMember u1 = new UniversityMember("Nova Cane", 21, "486573201");
+		System.out.println(u1);
 		
+		//Create a Professor
+		Professor pro1 = new Professor("Martin McDowell", 40, "182216283", "ZEI 300");
+		System.out.println(pro1);
+		
+		//Create a Student
+		Student s1 = new Student("Carol", 23, "954756321", 4.3);
+		System.out.println(s1);
 	}
 	
 }
